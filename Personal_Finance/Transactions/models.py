2340ba59bc2d2c437101e48be341853d919faacd
Revadi <rebrunda@909c4ac4a525.ant.amazon.com>
2022-09-11 18:00:47 +0530

    readme moved

--- Personal_Finance/Transactions/models.py
+++ Personal_Finance/Transactions/models.py
@@ -1,3 +1,54 @@
+from pydoc import describe
 from django.db import models
 
-# Create your models here.
+class Transactions(models.Model):
+    transaction_id=models.IntegerField(primary_key=True,max_length=100000)
+    transaction_date=models.DateField()
+    user_id=models.ForeignKey()
+    bank_id=models.ForeignKey()
+    description=models.CharField(max_length=500)
+    credit=models.FloatField()
+    debit=models.FloatField()
+    merchant=models.CharField(max_length=100)
+    expense_id=models.ForeignKey()
+
+class Assests(models.Model):
+    assest_id=models.IntegerField(primary_key=True)
+    user_id=models.ForeignKey()
+    assest_name=models.CharField(max_length=100)
+    assest_type=models.ForeignKey()
+    uom=models.CharField(max_length=100)
+    quantity=models.IntegerField()
+
+class Liabilites(models.Model):
+    liablities_id=models.IntegerField(primary_key=True)
+    user_id=models.ForeignKey()
+    liablities_name=models.CharField(max_length=100)
+    liablities_type=models.ForeignKey()
+    uom=models.CharField(max_length=100)
+    quantity=models.IntegerField()
+    
+class AssestType(models.Model):
+    assest_type_id=models.IntegerField()
+    assests_type=models.CharField(max_length=100)
+
+class LiabilitesType(models.Model):
+    liabilites_type_id=models.IntegerField()
+    liabilites_type=models.CharField(max_length=100)
+
+class ExpenseType(models.Model):
+    expense_type_id=models.IntegerField()
+    expense_type=models.CharField(max_length=100)
+
+class Bank(models.Model):
+    bank_id=models.IntegerField()
+    bank_name=models.CharField(max_length=100)
+    acc_no=models.IntegerField()
+    acc_type=models.CharField(max_length=50)
+    user_id=models.ForeignKey()
+
+class BankInfo(models.Model):
+    bank_info_id=models.IntegerField()
+    recepient=models.EmailField()
+    bank_id=models.ForeignKey()
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Finance Application
+
+Finance application is an all in one finance assisstance for an individual
+
+## Django
+ 
+Django is web framework
+
